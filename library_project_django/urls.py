6dd25133821836a2ccca4359dcfe94e63f011b7a
3oheil <143543953+3oheil@users.noreply.github.com>
2023-11-17 00:51:03 -0800

    all changes in library projects

--- library_project_django/urls.py
+++ library_project_django/urls.py
@@ -20,14 +20,15 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('home_module.urls')),
     path('', include('account_module.urls')),
+    path('', include('site_setting.urls')),
+    path('order/', include('order_module.urls')),
     path('products/', include('product_module.urls')),
     path('contact-us/', include('contact_module.urls')),
     path('articles/', include('article_module.urls')),
     path('user/', include('user_panel_module.urls')),
-    path('', include('site_setting.urls')),
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- static/js/custom.js
+++ static/js/custom.js
@@ -11,7 +11,62 @@
 // }
 
 
+/*
 function fillPage(page) {
     $('#page').val(page);
     $('#filter_form').submit();
 }
+
+
+function AddProductToOrder(productId) {
+    const productCount = $('#product-count').val();
+    $.get('/order/add-to-order?product_id=' + productId + '&count=' + productCount).then(res => {
+        if (res.status === 'success') {
+            Swal.fire({
+                title: 'اعلان',
+                text: res.text,
+                icon: res.icon,
+                showCancelButton: false,
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: res.confirmButtonText
+            });
+        } else if (res.status === 'not_found') {
+            Swal.fire({
+                title: 'اعلان',
+                text: res.text,
+                icon: res.icon,
+                showCancelButton: false,
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: res.confirmButtonText
+            });
+        } else if (res.status === 'not_auth') {
+            Swal.fire({
+                title: 'اعلان',
+                text: res.text,
+                icon: res.icon,
+                showCancelButton: false,
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: res.confirmButtonText
+            });
+        }
+    });
+}
+
+function RemoveOrderDetail(detailId) {
+    $.get('/user/remove-order-detail?detail_id=' + detailId).then(res => {
+        console.log(res);
+        // if (res.status === 'success'){
+        //     $('#order-detail-content').html(res.body);
+        // }
+    });
+}
+
+
+function ChangeOrderDetailCount(detailId, state){
+    // console.log(detailId, state);
+    $.get('/user/change-order-detail?detail_id=' + detailId + '&state=' + state).then(res => {
+        if (res.status === 'success') {
+            $('#order-detail-content').html(res.body);
+        }
+    });
+}*/
